584d63c66a1c3126f488fec06dd88ad36ed2def0
gerson <gersonrevatta@gmail.com>
2019-08-31 15:02:38 -0500

    [feature/user]Add link to my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @posts = Post.all
+    @posts = params[:my_posts].present? ? current_user.posts : Post.all
   end
 
   def new
