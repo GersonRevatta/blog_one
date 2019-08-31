0af24c11538b23556845177973d22082a34c5e8b
gerson <gersonrevatta@gmail.com>
2019-08-31 03:19:43 -0500

    [feature/user]Add user_id to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_081302) do
+ActiveRecord::Schema.define(version: 2019_08_31_081613) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_31_081302) do
     t.integer "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
